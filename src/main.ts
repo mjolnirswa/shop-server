6c6facfe5399b7a7e28736973200389249787c62
Matvey Melnik <mjolnirswa@gmail.com>
2023-07-09 12:01:07 +0300

    fix connect

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001', 'https://shop-client-e7m4.onrender.com/'],
+    origin: ['http://localhost:3001', 'https://shop-client-e7m4.onrender.com'],
   });
 
   const config = new DocumentBuilder()
